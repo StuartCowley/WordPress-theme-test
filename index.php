8ce3b9f106fdd3399f9cae8e1ff29b3bca11554d
StuartCowley <voidstalker@live.co.uk>
2020-10-06 23:27:53 +0100

    typography and general layout build

--- index.php
+++ index.php
@@ -13,19 +13,70 @@
 	</div>
 
 	<div class="main-content">
-		<div class="main-content__header-strip">
-			<div class="main-content__header-strip-logo">logo</div>
+		<div class="main-content__tab-wrap">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="main-content__tab-box">
+					<div class="main-content__tab-name">
+						<?php
+							$title = get_field("tab_title");
+							$words = explode(" ", $title);
+							foreach ($words as $word) {	?>
+								<div>
+									<?=$word?>
+								</div>
+						<?php };?>
+					</div>
+					<div class="main-content__tab-counter">
+						<?= "0" . get_field("tab_number");?>
+					</div>
+				</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+				<?php endif; ?>
 		</div>
 
+		<div class="main-content__horizontal-scroll-wrap">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class="main-content__post-wrap">
-			<h1><?= the_title(); ?></h1>
-			<h2><?= get_field("greeting");?> </h2>
-			<p><?= the_content(__('(more...)')); ?></p>
+			<div class="main-content__post-wrap">
+				<div class="main-content__rotate">
+					<div class="main-content__panel main-content__panel--wide">
+						<div class="main-content__panel-meta">
+							<div class="greeting-wrap">
+								<div class="greeting-progress">
+									<span>01</span>
+									<span class="greeting-progress-line"></span>
+									<span>02</span>
+								</div>
+								<div class="greeting">
+									<?= get_field("greeting");?>
+								</div>
+							</div>
+							<div class="title"><?= the_title(); ?></div>
+							<div class="subtitle"><?= get_field("subtitle");?> </div>
+
+							<div class="cta-wrap">
+								<div class="cta-arrow">
+									<?= file_get_contents(get_stylesheet_directory_uri() . '/assets/right-arrow.svg'); ?>
+								</div>
+								<div class="cta-text">
+									<?= (get_field("cta")['cta_label']); ?>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="main-content__panel">
+						<h2><?= get_field("featured_text");?> </h2>
+					</div>
+					<div class="main-content__panel">
+						3
+					</div>
+				</div>
+			</div>
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+				<?php endif; ?>
 		</div>
-		<?php endwhile; else: ?>
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-			<?php endif; ?>
 	</div>
 
 	<div class="sidebar-wrap sidebar--right">
